65908b4c21460e98dd24cd021a551bd5e4d3b6a8
Sacristan <girts.kesteris@gmail.com>
2019-08-28 19:50:49 +0300

    initial commit

---
+++ InteraktVR/CharacterControllerInteraction/Source/Interaction/InteractionCursorController.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using System.Collections;
+
+public enum CursorState { None = 0, Idle, Over, Clicked }
+
+public class InteractionCursorController : MonoBehaviour
+{
+    [SerializeField] private Texture defCursor;
+    [SerializeField] private Texture idleCursor;
+    [SerializeField] private Texture overCursor;
+    [SerializeField] private Texture clickedCursor;
+
+    [SerializeField] private LayerMask buttonLayerMask = ~0;
+
+    [SerializeField] private LayerMask objectPickupLayerMask = ~0;
+
+    Texture cursorImage;
+    private CursorState cursorState = CursorState.None;
+    [HideInInspector] public Vector2 cursorPos;
+
+    private bool isMouseDown = false;
+    private float maxDistance = 4.0f;
+    private Vector2 cursorSize = new Vector2(25, 25);
+
+    public CursorState CursorState
+    {
+        get => cursorState;
+
+        set
+        {
+            if (value != cursorState)
+            {
+                cursorState = value;
+                switch (cursorState)
+                {
+                    case CursorState.Over:
+                        cursorImage = overCursor;
+                        break;
+
+                    case CursorState.Clicked:
+                        cursorImage = clickedCursor;
+                        break;
+                    default:
+                        cursorImage = idleCursor;
+                        break;
+                }
+            }
+        }
+    }
+
+    IEnumerator Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        yield return null;
+        CursorState = CursorState.Idle;
+    }
+
+    private void OnGUI()
+    {
+        GUI.depth = -1;
+        Rect pos;
+
+        pos = new Rect(Screen.width / 2 - cursorSize.x / 2, Screen.height / 2 - cursorSize.y / 2, cursorSize.x, cursorSize.y);
+        cursorPos = new Vector2(pos.x, pos.y);
+        GUI.Label(pos, cursorImage);
+    }
+    ObjectItemMenuController objectItemMenuController;
+
+    //TODO: REMOVE ENTER / EXIT / CLICK MAGIC
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(cursorPos);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonUp(0)) isMouseDown = false;
+        if (isMouseDown) CursorState = CursorState.Clicked;
+
+        if (Physics.Raycast(ray, out hit, 100.0f, buttonLayerMask))
+        {
+            ObjectItemMenuController newObjectItemMenuController = hit.collider.gameObject.GetComponent<ObjectItemMenuController>();
+
+            if (newObjectItemMenuController != null)
+            {
+                if (newObjectItemMenuController != objectItemMenuController)
+                {
+                    if (objectItemMenuController != null) objectItemMenuController.Exit();
+                    objectItemMenuController = newObjectItemMenuController;
+                }
+
+                objectItemMenuController.Enter();
+            }
+        }
+        else
+        {
+            if (objectItemMenuController != null)
+            {
+                objectItemMenuController.Exit();
+                objectItemMenuController = null;
+            }
+        }
+
+        if (objectItemMenuController != null && Input.GetMouseButtonDown(0)) objectItemMenuController.Click();
+
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if (hit.collider.gameObject.IsInLayerMask(objectPickupLayerMask) && hit.distance < maxDistance)
+            {
+                if (!isMouseDown) CursorState = CursorState.Over;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    isMouseDown = true;
+                    CursorState = CursorState.Clicked;
+                }
+            }
+            else
+            {
+                if (!isMouseDown) CursorState = CursorState.Idle;
+            }
+        }
+    }
+
+}
---
+++ InteraktVR/SORT/VRInputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public static class VRInputManager
+{
+    private static bool m_isKeyPressed = false;         // true if controller button is pressed
+    private static Transform m_controllerTransform;     // controller transform
+    private static bool m_controllerActive = true;      // TOOD: actual value             // true if controller is active (movement should be tracked)
+
+    // public static void SetIsControllerButtonPressed(bool isPressed)
+    // {
+    //     m_isKeyPressed = isPressed;
+    // }
+
+
+    public static bool GetIsControllerButtonPressed()
+    {
+        // return m_isKeyPressed;
+        // return true;
+        return OVRInput.Get(OVRInput.RawButton.LIndexTrigger) || OVRInput.Get(OVRInput.RawButton.RIndexTrigger);
+    }
+
+    public static void SetControllerTransform(Transform transform)
+    {
+        m_controllerTransform = transform;
+    }
+
+    public static Transform GetControllerTransform()
+    {
+        return m_controllerTransform;
+    }
+
+    public static void SetControllerActive(bool active)
+    {
+        m_controllerActive = active;
+    }
+
+    public static bool GetControllerActive()
+    {
+        return m_controllerActive;
+    }
+}
