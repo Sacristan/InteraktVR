2b0f02b26ad78a1e89d749f6c2432291dfa53caf
Sacristan <girts.kesteris@gmail.com>
2019-10-23 17:32:01 +0300

    vr item collider fix redudant Awake

--- InteraktVR/Core/VRInteraction/Scripts/VRItemCollider.cs
+++ InteraktVR/Core/VRInteraction/Scripts/VRItemCollider.cs
@@ -1,33 +1,22 @@
-﻿//========= Copyright 2018, Sam Tague, All rights reserved. ===================
-//
-// Used for forwarding on trigger collision to a parent item
-//
-//===================Contact Email: Sam@MassGames.co.uk===========================
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace VRInteraction
 {
-	public class VRItemCollider : MonoBehaviour 
-	{
-		public VRInteractableItem item;
+    public class VRItemCollider : MonoBehaviour
+    {
+        [SerializeField] public VRInteractableItem item;
 
-		private Collider _col;
+        private Collider _col;
 
-		public Collider col
-		{
-			get 
-			{
-				if (_col == null) Awake();
-				return _col; 
-			}
-		}
-
-		void Awake()
-		{
-			_col = GetComponent<Collider>();
-		}
-	}
+        public Collider col
+        {
+            get
+            {
+                if (_col == null) _col = GetComponent<Collider>();
+                return _col;
+            }
+        }
+    }
 }
